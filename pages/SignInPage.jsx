afef46e916234219f984d990f036c7c77a45aba2
AbdusSalam777 <abdusalam0381@gmail.com>
2025-11-19 22:40:21 +0500

    Update SignInPage.jsx

--- pages/SignInPage.jsx
+++ pages/SignInPage.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { SignIn } from "@clerk/clerk-react";
-import { Navbar } from "../components/Navbar";
 
 const SignInPage = () => {
   return (
-    <>
-    <Navbar/>
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <SignIn appearance={{ elements: { formButtonPrimary: "bg-blue-600 hover:bg-blue-700" } }} />
+      <SignIn
+        path="/sign-in"
+        routing="path"
+        afterSignInUrl="/home"
+        appearance={{
+          elements: {
+            formButtonPrimary: "bg-blue-600 hover:bg-blue-700",
+          },
+        }}
+      />
     </div>
-    </>
-    
   );
 };
 
