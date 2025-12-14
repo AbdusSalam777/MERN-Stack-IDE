8f3b604c3012960490ed3c5c017342bf8947500c
AbdusSalam777 <abdusalam0381@gmail.com>
2025-12-14 16:08:00 +0500

    Integrate request tracking and history routes
    Added request tracking middleware and request history routes.

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-const PORT = 3000;
-import CourseRoutes from './routes/ProjectRoutes.js';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import CourseRoutes from "./routes/ProjectRoutes.js";
+import requestTracker from "./middlewares/requestStack.js";
+import requestRoutes from "./routes/requestHistory.js";
+
 dotenv.config();
 
-const app=express();
+const app = express();
+const PORT = 3000;
+
 app.use(cors());
 app.use(express.json());
+app.use(requestTracker);
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    console.log("MongoDB connected successfully!");
-}).
-catch((error)=>{
-    console.log(error);
-})
-    
-app.use("/projects",CourseRoutes);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected successfully!"))
+  .catch(err => console.log(err));
 
-app.get("/",async(req,res)=>{
-    res.send("Server running!")
+app.use("/projects", CourseRoutes);
+app.use("/req-history", requestRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server running!");
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server running on PORT ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
 });
